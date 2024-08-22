6fbd5271ae85b7a717bcff96708cb877179aa37e
sylvain kadjo <sylvainka12@gmail.com>
2024-08-23 01:10:01 +0400

    Project clean up
    - clean default content
    - add favicon
    - build project structure
    - reset global style
    - configure default font face

---
+++ src/lib/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Roboto } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({ weight: "400", subsets: ["latin"] });
+
+export { inter, roboto };
