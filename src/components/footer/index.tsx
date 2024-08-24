f3e83524e22bd0085d70517536b3f6a9a4825ef8
sylvain kadjo <sylvainka12@gmail.com>
2024-08-24 23:29:04 +0400

    Build footer component
    - add luxon library for date
    - create socials media list constant

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import { socialsLinks } from "@/lib/shared/constant";
+import { Icon } from "@iconify/react";
+import { DateTime } from "luxon";
+
+export default function FooterComponent() {
+  return (
+    <div className="footer">
+      <p className="footer__copyright">
+        &copy; {DateTime.now().year} SYLVAINKADJO.COM
+      </p>
+      <div className="footer__socials">
+        {socialsLinks &&
+          socialsLinks.map((link) => (
+            <Link
+              className="footer__socials-item"
+              key={link.label}
+              target="_blank"
+              href={link.link}
+            >
+              <Icon className="text-second" icon={link.icon} width={40} />
+            </Link>
+          ))}
+      </div>
+    </div>
+  );
+}
--- src/lib/shared/constant.ts
+++ src/lib/shared/constant.ts
@@ -1,5 +1,6 @@
 import { INavigationLinkItem } from "./interfaces";
 
+
 export const navMenuItems: INavigationLinkItem[] = [
   {
     name: "Home",
@@ -13,5 +14,28 @@ export const navMenuItems: INavigationLinkItem[] = [
   {
     name: "About",
     url: "/about",
+  },
+];
+
+export const socialsLinks = [
+  {
+    label: "Github",
+    icon: "uit:github-alt",
+    link: "https://github.com/sylvain12",
+  },
+  {
+    label: "Linkedin",
+    icon: "iconoir:linkedin",
+    link: "https://www.linkedin.com/in/sylvain-kadjo-374a4911b/",
+  },
+  {
+    label: "Instagram",
+    icon: "ph:instagram-logo-thin",
+    link: "https://www.instagram.com/sylvainka12/",
+  },
+  {
+    label: "Dribble",
+    icon: "ph:dribbble-logo-thin",
+    link: "https://dribbble.com/sylvain12",
   },
 ];
