014ca4768d10f41b5085f3f73c37fc0a91036886
sylvain kadjo <sylvainka12@gmail.com>
2024-09-01 01:12:51 +0400

    navbar style
    - add socials links to nav right
    - add full height to navbar child element
    - center nav menu

--- src/components/navbar/nav-right.tsx
+++ src/components/navbar/nav-right.tsx
@@ -1,11 +1,20 @@
 "use client";
 
 import Button from "@/components/ui/buttons";
+import { navSocialsLinks } from "@/lib/shared/constant";
 import { Icon } from "@iconify/react";
+import Link from "next/link";
 
 export default function NavRightComponent() {
   return (
     <div className="navbar__right">
+      <div className="navbar__right-socials">
+        {navSocialsLinks.map((social) => (
+          <Link key={social.label} target="_blank" href={social.link}>
+            <Icon icon={social.icon} width={24} />
+          </Link>
+        ))}
+      </div>
       <Button
         label="Subscribe"
         variant="second"
@@ -13,9 +22,9 @@ export default function NavRightComponent() {
         onClick={() => console.log("You clicked me!")}
       />
 
-      <div className="navbar__right-search">
+      {/* <div className="navbar__right-search">
         <Icon icon="circum:search" width={24} />
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/lib/shared/constant.ts
+++ src/lib/shared/constant.ts
@@ -38,3 +38,16 @@ export const socialsLinks = [
     link: "https://dribbble.com/sylvain12",
   },
 ];
+
+export const navSocialsLinks = [
+  {
+    label: "Github",
+    icon: "uit:github-alt",
+    link: "https://github.com/sylvain12",
+  },
+  {
+    label: "Linkedin",
+    icon: "iconoir:linkedin",
+    link: "https://www.linkedin.com/in/sylvain-kadjo-374a4911b/",
+  },
+];
