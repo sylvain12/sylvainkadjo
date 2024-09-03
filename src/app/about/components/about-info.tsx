0ac7346890c3109f40be6037f37437ed6bcc82e3
sylvain kadjo <sylvainka12@gmail.com>
2024-09-04 02:46:29 +0400

    fix about page profile picture

--- src/app/about/components/about-info.tsx
+++ src/app/about/components/about-info.tsx
@@ -4,35 +4,33 @@ import Button from "@/components/ui/buttons";
 import Image from "next/image";
 
 export default function AboutInfoComponent() {
-  return (
-    <div className="about__info">
-      <Image
-        width={150}
-        height={150}
-        className="about__info-profile rounded-full"
-        src="/assets/profile.jpg"
-        alt="Sylvain KADJO profile picture"
-        onLoad={() => <div>Loading...</div>}
-      />
-      <div className="about__info-name">
-        <h3>Here Sylvain KADJO 👋🏽🤗</h3>
-        <span>The guy with smile</span>
-      </div>
-
-      <p className="about__info-text">
-        I thrive on new challenges and love sharing my experience with others.
-        If you&lsquo;re looking for someone to collaborate on your next project
-        or simply want to exchange ideas, don&lsquo;t hesitate to reach out.
-        Let&lsquo;s connect !
-      </p>
-
-      {/* <Button
+return (
+  <div className="about__info">
+    <Image
+      width={150}
+      height={150}
+      className="about__info-profile"
+      src="sylvainkadjo-assets/sylvainkadjo_profile_picture.png"
+      alt="Sylvain KADJO profile picture"
+      onLoad={() => <div>Loading...</div>}
+    />
+    <div className="about__info-name">
+      <h3>Here Sylvain KADJO 👋🏽🤗</h3>
+      <span>The guy with smile</span>
+    </div>
+    <p className="about__info-text">
+      I thrive on new challenges and love sharing my experience with others. If
+      you&lsquo;re looking for someone to collaborate on your next project or
+      simply want to exchange ideas, don&lsquo;t hesitate to reach out.
+      Let&lsquo;s connect !
+    </p>
+    {/* <Button
         label="view more"
         onClick={() => console.log("You clicked me!")}
         transparent={true}
         variant="second"
         type="button"
       /> */}
-    </div>
-  );
+  </div>
+);
 }
